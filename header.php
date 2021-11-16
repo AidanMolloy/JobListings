1dbd6d0e5c72cd1b6173fafbe25ac8550825aaa6
Aidan Molloy <aidan@molloy.ie>
2021-11-16 16:52:05 +0000

    Navbar Styled

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
     <title>Jobs Listings</title>
 </head>
 <body>
-    <nav>
+    <nav>  
         <a href="/">Home</a>
         <a href="/insert_jobs.php">Post a Job</a>
         <a href="/drop_table.php">Delete Table</a>
